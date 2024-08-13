b95684f6ad95907498d3ed1b6157d8d3885a7e40
Fatima Shahzad <fatimashahzad062@gmail.com>
2024-08-14 03:07:22 +0500

    policy update

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -22,22 +22,48 @@ const Page = () => {
                             <p className='mb-3 mt-6'>{'Welcome to Heathrow Budget Parking ("we", "us", or "our"). This Privacy Policy outlines how we collect, use, and protect your personal information when you visit our website'} <a href="http://www.heathrowbudgetparking.co.uk" target='_blank'>http://www.heathrowbudgetparking.co.uk</a>{' or use our services. We are committed to safeguarding your privacy in accordance with the General Data Protection Regulation (GDPR) and other applicable UK data protection laws.'}</p>
 
                             
-                            <h4 className="text-[14px] font-bold text-gray-700 mb-2 mt-6">1. Information We Collect</h4>
-                            <p>{'We only collect the personal data that you provide to us directly:'}</p>
+                            <h4 className="text-[16px] font-bold text-gray-700 mb-0 mt-6">1. Information We Collect</h4>
+                            <p className='mb-2'>{'We only collect the personal data that you provide to us directly:'}</p>
 
-                            <p><b className='text-[12px] font-bold'>Contact Information:</b> Name, email address, and phone number.</p>
-                            <p><b className='text-[12px] font-bold'>Booking Details:</b> Information related to your booking, including dates and car information.</p>
-                            <p><b className='text-[12px] font-bold'>Payment Information:</b> Although we may handle payments through third-party payment processors, we do not store payment details ourselves.</p>
-                            <h4 className="text-[12px] font-bold text-gray-700 mb-2 mt-6">Usage Data</h4>
-                            <p>{"We may also collect information about how the Service is accessed and used Usage Data. This Usage Data may include information such as your computer's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data."}</p>
-                            <h3 className="text-[14px] font-bold text-gray-700 mt-6 mb-2">Tracking &amp; Cookies Data</h3>
-                            <p>We use cookies and similar tracking technologies to track the activity on our Service and hold certain information. Cookies are files with a small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyse our Service. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service. Examples of Cookies we use: Session Cookies. We use Session Cookies to operate our Service. Preference Cookies. We use Preference Cookies to remember your preferences and various settings. Security Cookies. We use Security Cookies for security purposes.</p>
-                            <h3 className="text-[14px] font-bold text-gray-700 mt-6 mb-2">Use of Data</h3>
-                            <p>CABBX LTD uses the collected data for various purposes: To provide and maintain the Service To notify you about changes to our Service To allow you to participate in interactive features of our Service when you choose to do so To provide customer care and support To provide analysis or valuable information so that we can improve the Service To monitor the usage of the Service To detect, prevent and address technical issues</p>
-                            <h3 className="text-[14px] font-bold text-gray-700 mt-6 mb-2">Transfer of Data</h3>
-                            <p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction. If you are located outside the United Kingdom and choose to provide information to us, please note that we transfer the data, including Personal Data, to the United Kingdom and process it there. Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer. CABBX LTD will take all steps reasonably necessary to ensure that your data is treated securely and under this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
-                            <h3 className="text-[14px] font-bold text-gray-700 mt-6 mb-2">Disclosure of Data</h3>
-                            <p><strong>Legal Requirements</strong> CABBX LTD may disclose your Data in the good faith belief that such action is necessary to: To comply with a legal obligation To protect and defend the rights or property of Xride LTD To prevent or investigate possible wrongdoing in connection with the Service To protect the personal safety of users of the Service or the public To protect against legal liability</p>
+                            <p><b className='text-[14px] font-bold'>Contact Information:</b> Name, email address, and phone number.</p>
+                            <p><b className='text-[14px] font-bold'>Booking Details:</b> Information related to your booking, including dates and car information.</p>
+                            <p><b className='text-[14px] font-bold'>Payment Information:</b> Although we may handle payments through third-party payment processors, we do not store payment details ourselves.</p>
+
+                            <h4 className="text-[16px] font-bold text-gray-700  mt-6">2. How We Use Your Information</h4>
+                            <p className='mb-2'>{'We use your personal data for the following purposes:'}</p>
+
+                            <p><b className='text-[14px] font-bold'>To Process Your Booking:</b> Handling reservations, managing your account, and providing customer support.</p>
+                            <p><b className='text-[14px] font-bold'>To Communicate with You:</b> Sending booking confirmations, updates, and relevant information related to your reservation.</p>
+                            <p><b className='text-[14px] font-bold'>To Improve Our Services:</b> Analyzing how you interact with our website to enhance user experience and service quality.</p>
+
+
+                            <h4 className="text-[16px] font-bold text-gray-700  mt-6">3. Legal Basis for Processing</h4>
+                            <p className='mb-2'>{'We process your personal data based on the following legal grounds:'}</p>
+
+                            <p><b className='text-[14px] font-bold'>Performance of a Contract:</b> To fulfill your booking and related services.</p>
+                            <p><b className='text-[14px] font-bold'>Legitimate Interests:</b> To manage our business effectively and improve our services.</p>
+                            <p><b className='text-[14px] font-bold'>Consent:</b> For sending any promotional communications, which you can withdraw at any time.</p>
+
+                            <h4 className="text-[16px] font-bold text-gray-700  mt-6">4. Sharing Your Information</h4>
+                            <p className='mb-2'>{'We may share your personal data with:'}</p>
+
+                            <p><b className='text-[14px] font-bold'>Service Providers:</b> Third-party vendors who assist with processing bookings and managing our website.</p>
+                            <p><b className='text-[14px] font-bold'>Legal Authorities:</b> When required by law or to protect our rights and the safety of others.</p>
+                           
+
+                        
+                            <h3 className="text-[16px] font-bold text-gray-700 mt-6 mb-2">5. Data Retention</h3>
+                            <p>We retain your personal data only as long as necessary to fulfill the purposes outlined in this Privacy Policy or as required by law. Typically, this means retaining your data for [specific period, e.g., "after deleting the account"] after your booking is complete.</p>
+
+                            <h4 className="text-[16px] font-bold text-gray-700  mt-6">6. Your Rights</h4>
+                            <p className='mb-2'>{'Under GDPR, you have the following rights regarding your personal data:'}</p>
+
+                            <p><b className='text-[14px] font-bold'>Right to Access:</b> Request a copy of the data we hold about you.</p>
+                            <p><b className='text-[14px] font-bold'>Right to Rectification:</b> Request corrections to any inaccurate or incomplete data.</p>
+                            <p><b className='text-[14px] font-bold'>Right to Erasure:</b> Request the deletion of your data under certain conditions.</p>
+                            <p><b className='text-[14px] font-bold'>Right to Restrict Processing:</b> Request that we limit how we process your data.</p>
+                            <p><b className='text-[14px] font-bold'>Right to Data Portability:</b> Request that we transfer your data to another service provider.</p>
+
                             <h3 className="text-[14px] font-bold text-gray-700 mt-6 mb-2">Security of Data</h3>
                             <p>The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.</p>
                             <h3 className="text-[14px] font-bold text-gray-700 mt-6 mb-2">Service Providers</h3>
